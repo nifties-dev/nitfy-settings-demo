cfc0a0890d8f880162c3ca006badae4cc70ef410
Andris Rauda <andris.rauda@substance.org.lv>
2024-04-23 22:52:23 +0300

    Initial commit

---
+++ src/main/java/dev/nifties/settings/demo/NiftySettingsDemoApplication.java
@@ -0,0 +1,13 @@
+package dev.nifties.settings.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NiftySettingsDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NiftySettingsDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/nifties/settings/demo/NiftySettingsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.nifties.settings.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NiftySettingsDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
