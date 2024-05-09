1259d20fad68d20bdf1a8c5d23d097a34f4b4edd
Andris Rauda <andris.rauda@substance.org.lv>
2024-05-09 22:48:15 +0300

    introduced abstract SettingsChannelBase class

--- src/main/java/dev/nifties/settings/demo/VolatileSettingsSource.java
+++ src/main/java/dev/nifties/settings/demo/VolatileSettingsSource.java
@@ -1,42 +1,26 @@
 package dev.nifties.settings.demo;
 
 import dev.nifties.settings.SettingValue;
-import dev.nifties.settings.SettingsChannel;
+import dev.nifties.settings.SettingsChannelBase;
 
-import java.util.*;
+import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.function.Consumer;
 
-public class VolatileSettingsSource implements SettingsChannel {
+public class VolatileSettingsSource extends SettingsChannelBase {
     private final Map<String, SettingValue> values = new ConcurrentHashMap();
-    private final Set<Consumer<String>> subscribers = Collections.synchronizedSet(new HashSet<>());
 
     public SettingValue get(String key) {
-        return (SettingValue)this.values.get(key);
+        return values.get(key);
     }
 
     public void put(String key, Object value) {
         SettingValue settingValue = new SettingValue(value);
-        this.values.put(key, settingValue);
-        this.notifySubscribers(key);
+        values.put(key, settingValue);
+        notifySubscribers(key);
     }
 
     public void remove(String key) {
-        this.values.remove(key);
-        this.notifySubscribers(key);
-    }
-
-    @Override
-    public void subscribe(Consumer<String> consumer) {
-        subscribers.add(consumer);
-    }
-
-    @Override
-    public void unsubscribe(Consumer<String> consumer) {
-        subscribers.remove(consumer);
-    }
-
-    protected void notifySubscribers(final String key) {
-        this.subscribers.forEach(s -> s.accept(key));
+        values.remove(key);
+        notifySubscribers(key);
     }
 }
